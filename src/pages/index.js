46c04e3229ebc4dbb3c5ba8aa6bfa1c91a2d823a
Harsh Kumar Pandey <coding.harshp@gmail.com>
2021-07-10 20:34:41 +0530

    Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h2>Develop & Deploy</h2>
           <p>UX designer & web developer based in West Bengal.</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
